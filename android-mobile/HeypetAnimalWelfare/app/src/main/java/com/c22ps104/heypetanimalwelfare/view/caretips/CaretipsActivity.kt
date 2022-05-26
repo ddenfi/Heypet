958bee09cad3bc409991d31d984f15b319b87e40
Irfan Arif Maulana <irfanams18@gmail.com>
2022-05-26 15:08:31 +0700

    Added activities

---
+++ android-mobile/HeypetAnimalWelfare/app/src/main/java/com/c22ps104/heypetanimalwelfare/view/caretips/CaretipsActivity.kt
@@ -0,0 +1,12 @@
+package com.c22ps104.heypetanimalwelfare.view.caretips
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c22ps104.heypetanimalwelfare.R
+
+class CaretipsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_caretips)
+    }
+}
